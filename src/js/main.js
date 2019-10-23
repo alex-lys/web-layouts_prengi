e2981a33fe0ba532028456832f4c5e66d8b0f5c9
Alex <la.prof@ya.ru>
2019-10-23 22:14:13 +0300

    add media rules for promo add media rules for header add media rules for menu add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
     //Slick Slider Promo
-    $(document).ready(function(){
-        $('.promo__slider').slick({
-            speed: 1200,
-            infinite: true,
-            arrows: true,
-            dots: true,
-            cssEase: 'ease',
-            appendArrows: $('.promo__nav'),
-            appendDots: $('.promo__dots'),
-            prevArrow: '<div class="promo__left"><img src="img/promo-slider-left.png" alt=""></div>',
-            nextArrow: '<div class="promo__right"><img src="img/promo-slider-right.png" alt=""></div>'
+    $('.promo__slider').slick({
+        speed: 1200,
+        infinite: true,
+        arrows: true,
+        dots: true,
+        cssEase: 'ease',
+        appendArrows: $('.promo__nav'),
+        appendDots: $('.promo__dots'),
+        prevArrow: '<div class="promo__left"><img src="img/promo-slider-left.png" alt=""></div>',
+        nextArrow: '<div class="promo__right"><img src="img/promo-slider-right.png" alt=""></div>'
 
-        });
     });
+
+    //Mobile Menu
+    // let buttonMenuMobile = $('.namburger'),
+    //     buttonMenuClose = $('.menu__close'),
+    //     menu = $('.menu');
+
+    const showMobileMenu = (element) => {
+        element.on('click', function() {
+            $('.menu').toggleClass('menu_active');
+        })
+    }
+
+    showMobileMenu($('.hamburger'));
+    showMobileMenu($('.menu__close'));
+
 
 });
